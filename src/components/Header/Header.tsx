9a5dd125e3a741ba46e46e7304d6d13ef07b4b75
Alena Batushkova <alena.batushkova@gmail.com>
2022-12-20 22:11:41 +0300

    add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.scss';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">UI elements</h1>
+      <h2 className="header__subtitle">Image Slider</h2>
+    </header>
+  );
+}
