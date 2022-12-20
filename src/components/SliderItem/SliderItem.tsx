00a364485f47e93a789ea5cc97eac0f38022e953
Alena Batushkova <alena.batushkova@gmail.com>
2022-12-20 22:19:37 +0300

    add slider item, styles

--- src/components/SliderItem/SliderItem.tsx
+++ src/components/SliderItem/SliderItem.tsx
@@ -1,10 +1,25 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React from 'react';
 import './SliderItem.scss';
 
-export const SliderItem = () => {
+export interface ISlideModel {
+  id: number;
+  url: string;
+  desc: string;
+}
+
+interface ISlideProps {
+  data: ISlideModel;
+}
+
+export const SliderItem = (props: ISlideProps) => {
+  const { url, desc } = props.data;
+
   return (
-    <div className="slider__item">
-      
+    <div className="slide">
+      <figure className="slide__figure">
+        <img src={url} alt={desc} className="slide__image" />
+        <figcaption className="slide__desc">{desc}</figcaption>
+      </figure>
     </div>
-  )
+  );
 }
