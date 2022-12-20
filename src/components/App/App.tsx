83c2008a6f1cdd8de7283311f131d35dc1152ed2
Alena Batushkova <alena.batushkova@gmail.com>
2022-12-20 22:12:29 +0300

    add styles, header component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Header } from '../Header/Header';
 import { Slider } from '../Slider/Slider';
 import './App.scss';
 
 function App() {
-  return <Slider />
+  return (
+    <div className="container">
+      <Header />
+      <Slider />
+    </div>
+  )
 }
 
 export default App;
